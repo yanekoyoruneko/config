b7a6cab8e8e040e8bfc18bb7aa66928b0209ab1b
yan3ku <wisniewskijas@outlook.com>
2024-12-12 16:58:17 +0100

    Emacs server things.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,11 +1,4 @@
-;;; .emacs --- personal configuration                -*- lexical-binding: t; -*-
-
-;; Everyone is permitted to do whatever they like with this software
-;; without limitation.  This software comes without any warranty
-;; whatsoever, but with two pieces of advice:
-;; - Be kind to yourself.
-;; - Make good choices.
-
+;;; init.el --- personal configuration                -*- lexical-binding: t; -*-
 ;;; Commentary:
 
 ;;            _____ __  __    _    ____ ____
@@ -17,9 +10,12 @@
 ;;            Escape-Meta-Alt-Control-Shift
 
 ;;; Code:
-;; SETUP ;;
-(setq gc-cons-threshold most-positive-fixnum)
+
+;; SETUP
+;;
+(setq gc-cons-threshold most-positive-fixnum) ; fix gc
 (add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (expt 2 23))))
+
 
 (eval-when-compile
   (defun emacs-path (path) (expand-file-name path user-emacs-directory))
@@ -30,117 +26,165 @@
 (use-package use-package
   :init
   (setq use-package-always-ensure t)
-  (setq use-package-enable-imenu-support t))
-
-
-(use-package emacs			; basics
+  (setq use-package-always-demand t)	; emacs server so load all at start
+  (setq use-package-enable-imenu-support t)
   :custom
+  (package-archives
+   '(("melpa"  . "https://melpa.org/packages/")
+     ("gnu"    . "https://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/"))))
+
+
+
+;; UI
+;;
+(use-package emacs
+  :custom
+  (custom-enabled-themes '(modus-operandi))
   (frame-title-format '("%b"))
   (menu-bar-mode nil)
   (tool-bar-mode nil)
   (inhibit-startup-screen t)
   (scroll-bar-mode nil)
-  (global-hl-line-mode t)
+  (global-hl-line-mode 't)
   (ring-bell-function 'ignore)
   (make-backup-files nil)
-  (create-lockfiles nil))
+  (create-lockfiles nil)
+  (display-line-numbers-type 'relative)
+  :hook
+  (prog-mode . display-line-numbers-mode)
+  (prog-mode . prettify-symbols-mode))
 
-
+
+;; BASIC
+;;
 (use-package emacs
+  :delight
+  (abbrev-mode " Abv" "abbrev")
+  (subword-mode)
   :bind
   ("<f12>"   . modus-themes-toggle)
-  ("C-\\"    . indent-region)
+					;  ("C-\\"    . indent-region)
   ("M-i"     . imenu)
   ("C-c C-c" . compile)
   ("C-x C-k" . kill-current-buffer)
+  ("C-c s"   . shell)
   ("C-c p"   . find-file-at-point)
   ("C-x C-b" . ibuffer)
-  ("C-x C-d" . dired)
-  ("C-c C-d" . eldoc)
-  ("C-x C-r" . recentf-open-files)
   ("C-?"     . undo-redo)
   ("C-/"     . undo-only)
-  :config
-  (windmove-default-keybindings)
-  (winner-mode)
   :custom
   (vc-follow-symlinks t)
   (disabled-command-function nil)
-  (custom-enabled-themes '(modus-vivendi))
   (browse-url-browser-function 'eww)	; usefull for hyperspec
   (global-subword-mode t)
-  (fill-column 80)
   (delete-selection-mode t)
   (repeat-mode t)
-  (comment-auto-fill-only-comments t)
-  (recentf-mode t)
   (savehist-mode t)
   (require-final-newline t)
   (save-place-mode t)
   (sentence-end-double-space nil)
   (global-auto-revert-mode t)
   (electric-pair-mode t)
-  (display-line-numbers-type 'relative)
   (tab-always-indent 'complete)
   (c-default-style "linux")
-  (package-archives
-   '(("melpa" . "https://melpa.org/packages/")
-     ("gnu" . "https://elpa.gnu.org/packages/")
-     ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
   :hook
   (before-save . delete-trailing-whitespace)
-  (prog-mode   . display-line-numbers-mode)
-  (prog-mode   . display-fill-column-indicator-mode)
-  (prog-mode   . auto-fill-mode)
-  (prog-mode   . prettify-symbols-mode)
   (prog-mode   . abbrev-mode))
 
+(use-package server
+  :ensure nil
+  :custom
+  (server-port 666)
+  (server-use-tcp t)
+  (server-name "demon"))
 
+
+;; WINDOWS
+(use-package windmove :config (windmove-default-keybindings))
+(use-package winner   :config (winner-mode))
+
+
+
+;; FILL
+;;
+(use-package emacs
+  :delight
+  (auto-fill-function " AF")
+  :custom
+  (fill-column 80)
+  (comment-auto-fill-only-comments t)
+  :hook
+  (prog-mode   . display-fill-column-indicator-mode)
+  (prog-mode   . auto-fill-mode))
+
+
+
+;; MINIBUFFER
+;;
 (use-package minibuffer
   :ensure nil
   :custom
-  (read-answer-short t)
+  (completions-format 'one-column)
+  (completions-header-format nil)
+  (completions-max-height 10)
+  (completion-auto-select nil)
   (use-short-answers t)
   (enable-recursive-minibuffers t)
   (minibuffer-depth-indicate-mode t)
   (minibuffer-electric-default-mode t)
   (completion-show-help nil)
   (completions-detailed t)
-  (completions-max-height 6)
   (completion-styles '(basic substring initials flex))
   (completion-category-overrides
    '((file (styles . (basic partial-completion)))
      (imenu (styles . (basic substring)))
-     (eglot (styles . (emacs22 substring))))))
+     (eglot (styles . (emacs22 substring)))))
+  :bind (:map completion-in-region-mode-map
+	      ("C-n" . minibuffer-next-completion)
+	      ("C-p" . minibuffer-previous-completion)
+	      ("C-j" . minibuffer-choose-completion)
+	      :map minibuffer-mode-map
+	      ("C-n" . minibuffer-next-completion)
+	      ("C-p" . minibuffer-previous-completion)))
 
 
 (use-package icomplete
-    :custom
-    (fido-mode t)
-    (fido-vertical-mode t))
-
-
-(use-package dabbrev
-  :bind (("M-/" . dabbrev-completion)
-         ("C-M-/" . dabbrev-expand))
   :custom
-  (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
+  (fido-mode t)
+  (fido-vertical-mode t))
+
+
+;; RECENTF
+;;
+(use-package recentf
+  :bind ("C-x C-r" . recentf-open-files)
+  :custom (recentf-mode t))
 
 
-(use-package erc
+
+;; EXPAND REGION
+(use-package expand-region
+  :bind ("M-2" . er/expand-region))
+
+
+
+;; DIRED
+;;
+(use-package dired
+  :ensure nil
+  :bind ("C-x C-d" . dired)
   :custom
-  (erc-server "irc.libera.chat")
-  ;; (erc-autojoin-mode t)
-  (erc-user-full-name "Yanek Sakura")
-  (erc-nick "Yan3ku")
-  (erc-autojoin-channels-alist '(("libera.chat" "#emacs")))
-  (erc-kill-buffer-on-part t)
-  (erc-join-buffer 'buffer)
-  (erc-auto-query 'bury)
-  :hook
-  (erc-mode . erc-spelling-mode))
+  (dired-dwim-target t)
+  (dired-auto-revert-buffer t)
+  (dired-listing-switches "-alh")
+  :config
+  (require 'dired-x))
 
 
+
+;; IM BAD IN SPELING
+;;
 (use-package flyspell
   :bind
   ("C-c i b" . flyspell-buffer)
@@ -151,10 +195,21 @@
   )
 
 
+(use-package dabbrev
+  :bind (("M-/" . dabbrev-completion)
+         ("C-M-/" . dabbrev-expand))
+  :custom
+  (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
+
+
+;; LSP
+;;
 (use-package eglot
+  :delight (eldoc-mode nil "eldoc")
   :bind (:map eglot-mode-map
-              ("C-c r" . eglot-rename)
-              ("C-c f" . eglot-format-buffer))
+	      ("C-c C-d" . eldoc)
+              ("C-c r"   . eglot-rename)
+              ("C-c f"   . eglot-format-buffer))
   :hook ((c-mode tuareg-mode python-mode) . eglot-ensure))
 
 
@@ -169,25 +224,61 @@
   :hook prog-mode)
 
 
-;;;;;;;;
-;; OCaml
-;;;;;;;;
+
+;; IRC
+;;
+(use-package erc
+  :custom
+  (erc-server "irc.libera.chat")
+  (erc-autojoin-mode t)
+  (erc-user-full-name "yanekoYORUNEKO")
+  (erc-nick "Yan3ku")
+  (erc-autojoin-channels-alist '(("libera.chat" "#emacs")))
+  (erc-kill-buffer-on-part t)
+  (erc-join-buffer 'buffer)
+  (erc-auto-query 'bury)
+  :hook
+  (erc-mode . erc-spelling-mode))
+
+
+(use-package eww
+  :custom (eww-auto-rename-buffer t))
+
+
+;; LISP
+;;
+(use-package highlight-quoted
+  :hook ((emacs-lisp-mode lisp-mode) . highlight-quoted-mode))
+
+(use-package easy-escape
+  :hook ((lisp-mode-hook emacs-lisp-mode) . easy-escape-minor-mode))
+
+(use-package paredit
+  :hook (((emacs-lisp-mode lisp-mode sly-mode) . paredit-mode)
+	 (sly-mrepl-mode . (lambda () (unbind-key "RET" 'paredit-mode-map)))))
+
+(use-package rainbow-delimiters
+  :hook
+  ((emacs-lisp-mode ielm-mode lisp-interaction-mode lisp-mode slime-repl-mode) .
+   rainbow-delimiters-mode))
+
+(use-package aggressive-indent
+  :hook ((emacs-lisp-mode lisp-mode) . aggressive-indent-mode))
+
+(use-package sly
+  :bind ("C-c C-z" . inferior-lisp)
+  :custom
+  (inferior-lisp-program "sbcl --noinform"))
+
+
+
+;; OCAML
+;;
 (use-package utop
   :custom (utop-command "dune utop . -- -emacs")
   :hook   (tuareg-mode . utop-minor-mode))
 (use-package merlin :hook (tuareg-mode . merlin-mode))
 (use-package tuareg)
 
-;;;;;;;;
-;; Lisp
-;;;;;;;;
-(use-package sly
-  :custom
-  (inferior-lisp-program "sbcl"))
-
-(use-package rainbow-delimiters
-  :hook
-  ((emacs-lisp-mode ielm-mode lisp-interaction-mode lisp-mode slime-repl-mode) .
-   rainbow-delimiters-mode))
 
 ;;; init.el ends here
